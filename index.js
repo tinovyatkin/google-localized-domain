d43293e2190f144edb755a22953ed1f685b37452
Tino Vyatkin <tino@vtkn.io>
2019-02-23 13:02:11 -0400

    format

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 'use strict';
 
-const { readFileSync } = require('fs');
 const path = require('path');
 const worldCountries = require('world-countries');
+const { readFileSync } = require('fs');
 
 /**
  * @type {Map.<string, string>}
  */
 const countryCodeToGoogleDomainMap = readFileSync(
   path.resolve(__dirname, './data/googleDomains.txt'),
-  'utf8'
-).split('\n').reduce((map, domain) => {
-  const country = worldCountries.find(({ tld }) =>
-    tld.some(t => domain.endsWith(t))
-  );
-  if (country) {
-    map.set(country.cca2, domain);
-  }
-  return map;
-}, new Map());
-
-
+  'utf8',
+)
+  .split('\n')
+  .reduce((map, domain) => {
+    const country = worldCountries.find(({ tld }) =>
+      tld.some(t => domain.endsWith(t)),
+    );
+    if (country) {
+      map.set(country.cca2, domain);
+    }
+    return map;
+  }, new Map());
 
 // create country code to top level domain map
 
 /**
  * Returns Google localized domain for a given country (if exists) or '.google.com'
- * 
+ *
  * @param {string} countryCode - ISO 3166-1 alpha-2 country code like 'GB'
  * @returns {string} - Google domain with leading dot, like '.google.ae'
  */
@@ -38,4 +38,4 @@ function getByCountryCode(countryCode) {
   }
   return '.google.com';
 }
-exports.getByCountryCode = getByCountryCode;
+module.exports.getByCountryCode = getByCountryCode;
